fd268b600a2d3378551abb429b5c2291109566a4
Tamás Ángyán <tamas.angyan@instructure.com>
2025-09-01 21:55:09 +0200

    removes Biome and readds eslint+prettier to both projects

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
 	schema: "./db/schema.ts",
 	dialect: "postgresql",
 	dbCredentials: {
-		// biome-ignore lint/style/noNonNullAssertion: <explanation>
 		url: process.env.DATABASE_URL!,
 	},
 });
